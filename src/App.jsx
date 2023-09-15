43130d97fc11818be6617e74b51acf5b1f33f51a
NankyaElsa <elsakayigwa@gmail.com>
2023-09-15 20:10:58 +0300

    first_in_branch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// import x from './x';
+// import trial from './trial';
+function App() {
+  return (
+<div>hello</div>    
+  );
+}
+
+export default App;
